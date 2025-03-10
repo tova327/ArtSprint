367b4db8e7a190b5bbc508146ddc8420772bb609
Tovi Yakovson <zivyaya865@gmail.com>
2025-03-10 18:38:19 +0200

    start the project

---
+++ server/Server.Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Core.models;
+
+public class DataContext : DbContext
+{
+    public DbSet<UserModel> Users { get; set; }
+    public DbSet<PaintingModel> Paintings { get; set; }
+    public DbSet<CompetitionModel> Competitions { get; set; }
+    public DbSet<CompetitionPaintingModel> CompetitionPaintings { get; set; }
+    public DbSet<CommentModel> Comments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(@"Server=localhost;Database=artsprint;user=root;password=1234;",
+            new MySqlServerVersion(new Version(8, 0, 21)));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configuring composite keys
+        modelBuilder.Entity<CompetitionPaintingModel>()
+            .HasKey(cp => new { cp.IdPaint, cp.IdCompetition });
+
+    }
+}
