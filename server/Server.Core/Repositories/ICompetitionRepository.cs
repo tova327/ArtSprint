c5c6c11d4c2d592395fb368870b5d6b966afc518
Tovi Yakovson <zivyaya865@gmail.com>
2025-03-17 00:14:01 +0200

    add all I repositories

---
+++ server/Server.Core/Repositories/ICompetitionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Server.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Core.Repositories
+{
+    public interface ICompetitionRepository
+    {
+         Task<CompetitionModel> GetCompetitionDetailsBySubjectAsync(ESubject subject);
+        Task<CompetitionModel> GetLastCompetitionAsync();
+    }
+}
