7b02624b28d73fdac8c0a910ac53843c4b3ccbb4
Tovi Yakovson <zivyaya865@gmail.com>
2025-03-17 00:43:40 +0200

    finish all repositories

---
+++ server/Server.Data/repositories/RepositoryManager.cs
@@ -0,0 +1,42 @@
+﻿using Server.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Data.repositories
+{
+    public class RepositoryManager:IRepositoryManager
+    {
+        private readonly IDataContext _context;
+
+        IUserRepository Users { get; }
+        ICompetitionRepository Competitions { get; }
+        ICompetitionPaintingRepository CompetitionPaintings { get; }
+        ICommentRepository Comments { get; }
+        IPaintingRepository Paintings { get; }
+
+        public RepositoryManager(
+            IDataContext context,
+            IUserRepository userRepository,
+           
+        ICompetitionRepository competitionsRepository,
+        ICompetitionPaintingRepository competitionPaintingRepository,
+        ICommentRepository commentsRepository,
+        IPaintingRepository paintingsRepository)    
+        {
+            _context = context;
+            Users = userRepository;
+            Competitions = competitionsRepository;
+            Paintings = paintingsRepository;
+            Comments = commentsRepository;
+            CompetitionPaintings= competitionPaintingRepository;
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
