5a0dcf4a5f659d66460114b403ad3d265a17d092
Tovi Yakovson <zivyaya865@gmail.com>
2025-03-17 01:04:44 +0200

    start with userService

---
+++ server/Server.Core/Services/IUserService.cs
@@ -0,0 +1,27 @@
+﻿using Server.Core.DTOs;
+using Server.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Core.Services
+{
+    public interface IUserService
+    {
+        Task<UserDTO> GetUserDetailsByIdAsync(int id);
+        Task UpdateUserNameAsync(int id, string name);
+        Task UpdateUserCameOnAsync(int id, DateTime cameOn);
+        Task UpdateUserIsMedalAsync(int id, bool isMedal);
+        Task UpdateUserLastPaintAsync(int id, DateTime? lastPaint);
+        Task<IEnumerable<UserDTO>> GetAllAsync();
+        Task<UserDTO?> GetByIdAsync(int id);
+
+        Task<UserDTO> AddAsync(UserDTO entity);
+
+        Task<UserDTO> UpdateAsync(int id, UserDTO entity);
+
+        Task DeleteAsync(int id);
+    }
+}
