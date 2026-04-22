18d0e277ef5445185773e44b8c0f6fe3785cdb8f
Tovi Yakovson <zivyaya865@gmail.com>
2026-04-22 23:43:01 +0300

    fix paintingPostModel

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -25,7 +25,7 @@ internal class Program
         {
             options.AddPolicy("AllowAll", builder =>
             {
-                builder.WithOrigins("https://artsprint.onrender.com")
+                builder.WithOrigins("https://artsprint.onrender.com", "http://localhost:5173/")
                        .AllowAnyMethod()
                        .AllowAnyHeader()
                        .AllowCredentials();
