909238eec2606d5d6429a74edb21ad88a8798e8d
Tovi Yakovson <zivyaya865@gmail.com>
2026-06-07 00:09:52 +0300

    add env for server api url

--- Admin-pannel/admin-panel/src/api/axiosClient.ts
+++ Admin-pannel/admin-panel/src/api/axiosClient.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://artsprintserver.onrender.com/api",
+  baseURL: import.meta.env.VITE_MY_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
