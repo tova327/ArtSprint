611f45de09f955dcba1b34ee34ed109ccabb0205
Tovi Yakovson <zivyaya865@gmail.com>
2026-06-08 23:29:09 +0300

    only router in main

--- Admin-pannel/admin-panel/src/main.tsx
+++ Admin-pannel/admin-panel/src/main.tsx
@@ -4,13 +4,14 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Toaster } from 'sonner'
-import Router from './routs/Router.tsx'
 import AuthProvider from './components/auth/AuthProvider.tsx'
+
+import Router from './routs/Router.tsx';
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode><>
   <QueryClientProvider client={queryClient}>
-  <AuthProvider>   <Router />
+  <AuthProvider>   <Router/>
     <Toaster /></AuthProvider></QueryClientProvider>
  </>
   </StrictMode>,
